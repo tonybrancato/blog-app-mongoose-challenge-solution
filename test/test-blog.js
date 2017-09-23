d6a9461765e47cd14e8f8db748e25015ca0e0015
Tony Brancato <tonybrancato1@gmail.com>
2017-09-23 12:09:54 -0400

    GET finished not working

--- test/test-blog.js
+++ test/test-blog.js
@@ -7,8 +7,119 @@ const mongoose = require('mongoose');
 // this module
 const should = chai.should();
 
-const {Restaurant} = require('../models');
+const {DATABASE_URL} = require('../config');
+const {BlogPost} = require('../models');
 const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
+
+function seedBlogData() {
+  console.info('seeding blog data');
+  const seedData = [];
+
+  for (let i=1; i<=10; i++) {
+    seedData.push(generateBlogData());
+  }
+  // this will return a promise
+  return BlogPost.insertMany(seedData);
+}
+
+function generateBlogData() {
+	console.info('seeding post data');
+  return {
+    author: {
+    	firstName: faker.name.firstName(),
+    	lastName: faker.name.lastName()
+    },
+    title: faker.lorem.sentence(),
+    content: faker.lorem.text(),
+	}
+}
+
+describe('GET endpoint', function() {
+
+    it('should return all existing blog posts', function() {
+      // strategy:
+      //    1. get back all blog psots returned by by GET request to `/posts`
+      //    2. prove res has right status, data type
+      //    3. prove the number of blog posts we got back is equal to number
+      //       in db.
+      //
+      // need to have access to mutate and access `res` across
+      // `.then()` calls below, so declare it here so can modify in place
+      let res;
+      return chai.request(app)
+        .get('/posts')
+        .then(function(_res) {
+          // so subsequent .then blocks can access resp obj.
+          res = _res;
+          res.should.have.status(200);
+          // otherwise our db seeding didn't work
+          res.body.posts.should.have.length.of.at.least(1);
+          return BlogPost.count();
+        })
+        .then(function(count) {
+          res.body.posts.should.have.length.of(count);
+        });
+    });
+
+
+    it('should return restaurants with right fields', function() {
+      // Strategy: Get back all restaurants, and ensure they have expected keys
+
+      let resBlogPosts;
+      return chai.request(app)
+        .get('/posts')
+        .then(function(res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.posts.should.be.a('array');
+          res.body.posts.should.have.length.of.at.least(1);
+
+          res.body.forEach(function(post) {
+            post.should.be.a('object');
+            post.should.include.keys('content', 'title', 'author', 'id', 'created');
+          });
+          resBlogPosts = res.body.posts[0];
+          return BlogPost.findById(resBlogPosts.id);
+        })
+        .then(function(post) {
+          resBlogPosts.author.should.equal(blogPost.author);
+          resBlogPosts.title.should.equal(blogPost.title);
+          resBlogPosts.content.should.equal(blogPost.content);
+        });
+    });
+  });
+
+
+// this function deletes the entire database.
+// we'll call it in an `afterEach` block below
+// to ensure  ata from one test does not stick
+// around for next one
+function tearDownDb() {
+    console.warn('Deleting database');
+    return mongoose.connection.dropDatabase();
+}
+
+describe('Blog API resource', function() {
+
+  // we need each of these hook functions to return a promise
+  // otherwise we'd need to call a `done` callback. `runServer`,
+  // `seedRestaurantData` and `tearDownDb` each return a promise,
+  // so we return the value returned by these function calls.
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function() {
+    return seedBlogData();
+  });
+
+  afterEach(function() {
+    return tearDownDb();
+  });
+
+  after(function() {
+    return closeServer();
+  })
